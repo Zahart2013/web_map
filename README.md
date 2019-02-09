8201d16109d8604c150e9785116fe3f69afd9428
Marko <zahart76@gmail.com>
2019-02-09 21:02:37 +0200

    Readme update

---
+++ README.md
@@ -0,0 +1,35 @@
+<h2>Web_map<br></h>
+
+Module generates .html file, that can create map with population and films' filming locations.
+<h2>Installation<br></h>
+
+You need to install everything from <b>requirements.txt</b> file via command
+~~~~
+sudo pip install -r  requirements.txt
+~~~~
+<h2>Usage<br></h>
+
+Start the application<br>
+~~~~
+python3 web_map.py
+~~~~
+You will need to enter year to generate map with films for that year.
+
+In case of<br><i>__geopy.exc.GeocoderQuotaExceeded: HTTP Error 429: Too Many Requests__</i><br>
+try to change your WI-Fi network or use VPN service
+
+Then you will get <b>Map_{year}.html</b>
+
+<h2> HTML file description<br></h2>
+
+first tag `<!DOCTYPE HTML>` directly define that file type is html5<br>
+`<head>` contains all technique information of the document<br>
+`<meta>` contains an <i>meta</i> tags, which contain information for browsers and search systems<br> 
+`<script>` contains references to program or its text<br>
+`<link>`  sets connection with external documents<br>
+`<style>` defines styles of elements of the page<br>
+`<body>` contains all maintain of web-page. In this case is short because all is performed in script<br>
+After this file contains code to generate the map itself.
+
+<h2> Conclusion<br></h2>
+We can use Python based frameworks like <b>folium</b> to generate .html files for our web applications. It is simple and fast to use, so it can be effective instrument in development.
---
+++ web_map/web_map.py
@@ -0,0 +1,88 @@
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+import folium
+import progressbar
+
+
+def mapa_build():
+    """
+    Builds html file with map with two layers(population and films by year)
+    """
+    year = input()
+    mapa = folium.Map(tiles="Mapbox Bright", zoom_start=1000)
+    mapa.add_child(population_layer())
+    mapa.add_child(film_by_year_layer(collect_data("locations.list", year), year))
+    mapa.add_child(folium.LayerControl())
+    mapa.save('Map_'+year+'.html')
+
+
+def population_layer():
+    """
+    (None) -> FeatureGroup
+    Builds population layer
+    """
+    fg = folium.FeatureGroup(name="Population")
+    fg.add_child(folium.GeoJson(data=open('world.json', 'r',
+                                encoding='utf-8-sig').read(),
+                                style_function=lambda x: {'fillColor':'green'
+                                if x['properties']['POP2005'] < 10000000
+                                else 'orange'
+                                if 10000000 <= x['properties']['POP2005'] < 20000000
+                                else 'red'}))
+    return fg
+
+
+def film_by_year_layer(data, year):
+    """
+    (list, str) -> FeatureGroup
+    Builds layer with films on locations by user's year
+    """
+    fg = folium.FeatureGroup(name="Filmed in " + year)
+    bar = progressbar.ProgressBar(max_value=len(data))
+    i = 0
+    for each in data:
+        try:
+            fg.add_child((folium.Marker(location=generate_coordinates(each[1]),
+                                        popup=each[0],
+                                        icon=folium.Icon())))
+            bar.update(i)
+            i += 1
+        except:
+            print("Error\n")
+            bar.update(i)
+            i += 1
+            continue
+    return fg
+
+
+def collect_data(file_name, year):
+    """
+    (str, str) -> list
+    Gets .list file and year, returns list with tuples (film name, location)
+    """
+    with open(file_name, 'r', encoding="Latin-1") as file:
+        film_data = []
+        bar = progressbar.ProgressBar(max_value=1241786)
+        i = 0
+        for line in file.readlines():
+            if "\t" in line and line.split("(")[1][:4] == year:
+                film_data.append((line.split("\t")[0],
+                                  line.split("\t")[-1][:-1]))
+            bar.update(i)
+            i += 1
+    return film_data
+
+
+def generate_coordinates(location):
+    """
+    (str) -> list
+    Gets location name and returns list with coordinates
+    """
+    geolocator = Nominatim(user_agent="web_map")
+    geocode = RateLimiter(geolocator.geocode, min_delay_seconds=0.1)
+    coordinate = geolocator.geocode(location)
+    return [coordinate.latitude, coordinate.longitude]
+
+
+if __name__ == '__main__':
+    mapa_build()
